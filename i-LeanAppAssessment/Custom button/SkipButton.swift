5370b15f0a0ef07e00dac9f6d56ef5fd2baeb313
Shrushti S Shetty <shrushti.shetty@robosoftin.com>
2022-12-22 18:26:03 +0530

    first commit shrushti

--- i-LeanAppAssessment/Custom button/SkipButton.swift
+++ i-LeanAppAssessment/Custom button/SkipButton.swift
@@ -2,43 +2,78 @@
 //  SkipButton.swift
 //  i-LeanAppAssessment
 //
-//  Created by Harsha R Mundaragi on 11/12/22.
+//  Created by Shrushti Shetty on 22/12/22.
 //
 
 import Foundation
 import UIKit
+
 class SkipCustomButton: UIButton{
+
     
+
     
+
     override init(frame: CGRect) {
+
+ 
 
         super.init(frame: frame)
 
+ 
+
     }
+
+ 
 
     required init?(coder: NSCoder) {
 
+ 
+
         super.init(coder: coder)
+
         roundCorners()
+
     }
+
+ 
 
     func roundCorners() {
 
+ 
+
         let yourAttributes: [NSAttributedString.Key: Any] = [
+
               .font: UIFont.systemFont(ofSize: 16),
+
               .underlineStyle: NSUnderlineStyle.single.rawValue
+
           ]
+
         
+
         let attributeString = NSMutableAttributedString(
+
                 string: "Skip",
+
                 attributes: yourAttributes
+
              )
+
         
+
         self.setAttributedTitle(attributeString, for: .normal)
+
 //        self.titleLabel?.font = UIFont(name: "Rubik Medium", size: 16)
 
+ 
+
         }
+
     
+
    
+
+ 
 
 }
--- i-LeanAppAssessment/Key chain/KeyChainFIle.swift
+++ i-LeanAppAssessment/Key chain/KeyChainFIle.swift
@@ -2,58 +2,104 @@
 //  KeyChainFIle.swift
 //  i-LeanAppAssessment
 //
-//  Created by Harsha R Mundaragi on 12/12/22.
+//  Created by Shrushti Shetty on 22/12/22.
 //
 
 import Foundation
-
-
 class KeyChain {
 
-    func saveData(userName: String, data: Data)  {
+    
+
+    func saveData(userId: String, data: Data)  {
+
+        
 
         let query = [
 
-            kSecClass as String       : kSecClassGenericPassword,
+            
 
-            kSecAttrAccount as String : userName,
+            kSecClass as String: kSecClassGenericPassword,
 
-            kSecValueData as String   : data ] as [String : Any]
+            
+
+            kSecAttrAccount as String: userId,
+
+            
+
+            kSecValueData as String: data ] as [String : Any]
+
+   
 
         _ = SecItemAdd(query as CFDictionary, nil)
 
+ 
+
     }
 
-    func loadData(userName: String) -> Data? {
+ 
+
+    func loadData(userId: String) -> Data? {
+
+        
 
         let query = [
 
-            kSecClass as String       : kSecClassGenericPassword,
+            
 
-            kSecAttrAccount as String : userName,
+            kSecClass as String: kSecClassGenericPassword,
 
-            kSecReturnData as String  : kCFBooleanTrue!,
+            
 
-            kSecMatchLimit as String  : kSecMatchLimitOne
+            kSecAttrAccount as String: userId,
+
+            
+
+            kSecReturnData as String: kCFBooleanTrue!,
+
+            
+
+            kSecMatchLimit as String: kSecMatchLimitOne
+
+            
 
         ] as [String: Any]
 
         
 
+        
+
+        
+
         var result: AnyObject? = nil
+
+        
 
         let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &result)
 
+        
+
         if status == noErr {
+
+            
 
             return result as! Data?
 
+            
+
         } else {
+
+            
 
             return nil
 
+            
+
         }
 
+        
+
     }
+
+    
 
 }
